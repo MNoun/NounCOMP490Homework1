047306c94538b6326555aff98dadf67ced812baf
MNoun <mnoun@student.bridgew.edu>
2022-04-25 14:32:12 -0400

    Created outline for program

---
+++ main.py
@@ -0,0 +1,26 @@
+# Created by: Mitchell Noun
+# Date created: 4/21/22
+# Class: COMP490 Senior Design and Development
+# Assignment: Homework 1
+from dataclasses import dataclass
+
+
+@dataclass
+class ItemData:
+    name: str
+    price: float
+    type: str
+
+
+
+def main():
+
+    return
+
+
+def calculateTotalCharge():
+    return
+
+
+if __name__ == '__main__':
+    main()
